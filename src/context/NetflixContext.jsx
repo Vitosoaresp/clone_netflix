fb2b64ac0b435a0de2cc701ea88615392a775e87
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-30 16:49:00 -0300

    Feat: adc função para mudar a myList e salva ela no localStorage

--- src/context/NetflixContext.jsx
+++ src/context/NetflixContext.jsx
@@ -1,13 +1,12 @@
-import { createContext, useEffect, useState } from 'react';
-import FetchTMDB from '../service/FetchTMDB';
+import { createContext, useEffect, useState } from "react";
+import FetchTMDB from "../service/FetchTMDB";
 export const context = createContext();
-
 
 export function NetflixContextProvider({ children }) {
   const [listMovies, setListMovies] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [isTransparentHeader, setIsTransparentHeader] = useState(true);
-  const [myList, setMyList] = useState([]);
+  const [myList, setMyList] = useState(JSON.parse(localStorage.getItem('myListcloneNetflix')) || []);
 
   useEffect(() => {
     const fetchDataMovies = async () => {
@@ -17,31 +16,54 @@ export function NetflixContextProvider({ children }) {
     fetchDataMovies();
 
     const scroollListener = () => {
-      if(window.scrollY > 100) {
+      if (window.scrollY > 100) {
         setIsTransparentHeader(false);
       } else {
         setIsTransparentHeader(true);
       }
-    }
+    };
 
-    window.addEventListener('scroll', scroollListener);
+    window.addEventListener("scroll", scroollListener);
     return () => {
-      window.removeEventListener('scroll', scroollListener);
-    }
+      window.removeEventListener("scroll", scroollListener);
+    };
   }, []);
 
   useEffect(() => {
-    if(listMovies.length > 0) {
-      const originals = listMovies.filter((category) => category.slug === 'originals');
-      const random = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+    if (listMovies.length > 0) {
+      const originals = listMovies.filter(
+        category => category.slug === "originals"
+      );
+      const random = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       const chosen = originals[0].items.results[random];
       const getMovieInfo = async () => {
-        const data = await FetchTMDB.fetchMovieInfo(chosen.id, 'tv');
+        const data = await FetchTMDB.fetchMovieInfo(chosen.id, "tv");
         setFeaturedData(data);
-      }
+      };
       getMovieInfo();
     }
   }, [listMovies]);
 
-  return <context.Provider value={{listMovies, featuredData, isTransparentHeader}}>{children}</context.Provider>;
+  useEffect(() => {
+    localStorage.setItem("myListcloneNetflix", JSON.stringify(myList));
+  }, [myList]);
+
+  const changeMyList = movieData => {
+    const isOnTheList = myList.find(film => film.id === movieData.id);
+    if (isOnTheList) {
+      const newList = myList.filter(film => film.id !== movieData.id);
+      return setMyList(newList);
+    }
+    setMyList([...myList, { ...movieData }]);
+  };
+
+  return (
+    <context.Provider
+      value={{ listMovies, featuredData, isTransparentHeader, changeMyList, myList }}
+    >
+      {children}
+    </context.Provider>
+  );
 }
