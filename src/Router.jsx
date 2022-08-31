e818b3494924905c3fd81130c262e7f5fa13d71e
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-30 21:04:16 -0300

    add route /myList

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Browser } from './pages/Browser';
 import { MovieTrailer } from './pages/MovieTrailer';
+import { MyList } from './pages/MyList';
 import { Profile } from './pages/Profile';
 
 export function Router() {
@@ -8,6 +9,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<Browser />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/myList" element={<MyList />} />
       <Route path="/movie/:id/:key" element={<MovieTrailer />} />
     </Routes>
   )
